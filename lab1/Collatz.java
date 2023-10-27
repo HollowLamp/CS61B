21e025e253aba4a20acc92a0df69fab6e83c8195
HollowLamp <2504384479@qq.com git config --global user.name zhuB88exit()git config --global user.email 2504384479@qq.com>
2023-10-27 15:39:02 +0800

    lab1 done

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,13 +5,9 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+        if (n%2==0)
+            return n/2;
+        else return n*3+1;
     }
 
     public static void main(String[] args) {
@@ -24,3 +20,4 @@ public class Collatz {
         System.out.println();
     }
 }
+
